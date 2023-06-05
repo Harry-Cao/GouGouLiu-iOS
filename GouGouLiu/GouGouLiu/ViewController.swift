cdde0c343c4a0d10d6a3909767768dfc06860ba4
HarryCao <61426193+Harry-Cao@users.noreply.github.com>
2023-06-05 22:31:57 +0800

    initialize

---
+++ GouGouLiu/GouGouLiu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GouGouLiu
+//
+//  Created by Harry Cao on 2023/6/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
