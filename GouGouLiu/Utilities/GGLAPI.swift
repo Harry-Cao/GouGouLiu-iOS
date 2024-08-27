73dcb34b1e33534eb038f109972b766e49d275ff
Harry Cao <V176HAC@cathaypacific.com>
2024-08-27 15:26:18 +0800

    optimize GGLAPI

--- GouGouLiu/Utilities/GGLAPI.swift
+++ GouGouLiu/Utilities/GGLAPI.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct GGLAPI {
     static let host: String = UserDefaults.host.rawValue
-    static let baseURL: URL = URL(string: "http://\(host)")!
+    static let baseURL: URL = URL(string: host)!
 }
 
 extension GGLAPI {
     enum Host: String, CaseIterable {
-        case internet = "f3.ttkt.cc:15791"
-        case intranet = "192.168.0.211:8888"
+        case internet = "http://f3.ttkt.cc:15791"
+        case intranet = "http://192.168.0.211:8888"
     }
 }
 
